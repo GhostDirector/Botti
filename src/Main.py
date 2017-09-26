ba723d43ce3d62a7ad3ae41fe2afc3d7d36f0f9c
Joni Tuominen <joni.tuominen@cs.tamk.fi>
2017-09-26 18:32:27 +0300

    Add class Disco

--- src/Main.py
+++ src/Main.py
@@ -3,29 +3,33 @@ import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 
-# Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="DISCOBOT 2000", command_prefix="-", pm_help=True)
+class Disco:
+    # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
+    global client
+    client = Bot(description="DISCOBOT 2000", command_prefix="-", pm_help=True)
 
 
-# Suorittaa tämän metodin aina käynnistyksessä
-@client.event
-async def on_ready():
-    print('Logged in as ' + client.user.name + ' (ID:' + client.user.id + ') | Connected to ' + str(
-        len(client.servers)) + ' servers | Connected to ' + str(len(set(client.get_all_members()))) + ' users')
-    print('HELLO')
+    # Suorittaa tämän metodin aina käynnistyksessä
+    @client.event
+    async def on_ready(*args):
+        print('Logged in as ' + client.user.name + ' (ID:' + client.user.id + ') | Connected to ' + str(
+            len(client.servers)) + ' servers | Connected to ' + str(len(set(client.get_all_members()))) + ' users')
+        print('HELLO')
 
 
-# Tekstin tulostus
-@client.command()
-async def testPrint(*args):
-    await client.say("Testing")
-    await asyncio.sleep(3)
-    await client.say("Another testing")
+    # Tekstin tulostus
+    @client.command()
+    async def testPrint(*args):
+        await client.say("Testing")
+        await asyncio.sleep(3)
+        await client.say("Another testing")
 
-@client.command()
-async def testPrint2(*args):
-    await client.say("Testing2")
+    @client.command()
+    async def testPrint2(*args):
+        await client.say("Testing2")
 
 
-client.run('MzYyMTc2Mjc5MjM3MjMwNTky.DKu2hQ.opMSFRUxngL_P1uNMjN5gyVHdd8')
+    client.run('MzYyMTc2Mjc5MjM3MjMwNTky.DKu2hQ.opMSFRUxngL_P1uNMjN5gyVHdd8')
 
+if __name__ == "__main__":
+    Disco()
