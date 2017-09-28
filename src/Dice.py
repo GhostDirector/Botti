31555ab898199d78a4378798cb43067e81904c1b
Jorix3 <jyri.virtaranta@cs.tamk.fi>
2017-09-28 15:41:40 +0300

    add roll method

--- src/Dice.py
+++ src/Dice.py
@@ -3,10 +3,13 @@ from random import randint
 
 class Dice:
     def __init__(self):
-        min = 1
-        max = 100
+        self.min = 1
+        self.max = 100
 
-    def print(self, min, max):
+    def print_roll(self, min, max):
         print("(", min, "-", max, ") Result: ", randint(min, max))
+
+    def roll(self):
+        self.print_roll(self.min, self.max)
 
     
