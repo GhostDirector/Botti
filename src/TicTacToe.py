d11a7a426f40177bd17d41c2ceb2a6fc6338c325
Joni Tuominen <joni.tuominen@cs.tamk.fi>
2017-10-01 14:37:26 +0300

    make some changes to methods and add testPrint method

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -2,32 +2,52 @@ from random import randint
 
 class TicTacToe:
 
-    __board = [3][3]
+    __board = []
+
+    def __init__(self):
+        for i in range(9):
+            self.__board.append('-')
 
     def sendBoard(self):
         print()
 
+    def testPrint(self):
+        for x in range(0, len(self.__board)):
+            print(self.__board[x])
+        print("########################################################")
+
     def resetGame(self):
-        for i in self.__board:
-            for j in self.__board:
-                self.__board[i][j] = '-'
+        for x in range(0, len(self.__board)):
+            self.__board[x] = '-'
 
 
-    def makeMove(self, row, col):
-        if self.isValid(row, col) & self.isFree(row, col):
-            self.__board[row][col] = 'O'
+    def makeMove(self, spot):
+        if self.isValid(spot) & self.isFree(spot):
+            self.__board[spot] = 'O'
 
     def checkWinner(self):
         print()
 
-    def isFree(self, row, col):
-        if self.__board == '-':
+    def isFree(self, spot):
+        if self.__board[spot] == '-':
             return True
 
-    def isValid(self, row, col):
-        if row >= 0 & row <= 3:
-            if row >= 0 & row <= 3:
-                return True
+    def isValid(self, spot):
+        if spot >= 0 & spot <= 9:
+            return True
 
     def botMove(self):
-        self.__board[randint(0, 2)][randint(0, 2)] = 'X'
+        self.__board[randint(0, 2)] = 'X'
+
+    def Main(self):
+        self.resetGame()
+        self.sendBoard()
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.resetGame()
+    game.testPrint()
+    game.makeMove(4)
+    game.testPrint()
+    game.botMove()
+    game.testPrint()
